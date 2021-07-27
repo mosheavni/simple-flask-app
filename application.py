3002d4c380c74b54b6d0964926837f38b34fd8a0
Moshe Avni <moshe@avni.us>
2021-07-27 14:33:35 +0300

    Change color of the header

--- application.py
+++ application.py
@@ -2,8 +2,11 @@ from flask import Flask
 application = Flask(__name__)
 
 # print a nice greeting.
-def say_hello(username = "World"):
-    return '<h1 style="color:red;">Hello %s!</h1>\n' % username
+
+
+def say_hello(username="World"):
+    return '<h1 style="color:blue;">Hello %s!</h1>\n' % username
+
 
 # some bits of text for the page.
 header_text = '''
@@ -18,12 +21,11 @@ footer_text = '</body>\n</html>'
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+                                        say_hello() + instructions + footer_text))
 
 # add a rule when the page is accessed with a name appended to the site URL.
 application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+                                                  header_text + say_hello(username) + home_link + footer_text))
 
 if __name__ == "__main__":
     application.run()
-
